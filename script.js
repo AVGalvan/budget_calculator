95d606cd24ab20ad8f742bdf1c141c3f24bd42b7
Axel Galvan <axelgalvan0126@gmail.com>
2022-01-29 10:13:56 -0800

    Add updatedDocSavings method to pay object & make custom percentage input integers

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const pay = {
     payCheck: 0.00,//total money earned.
-    shortSavings: 0.00,//current amount of money in short term savings (see google docs).
-    longSavings: 0.00,//current amount of money in long term savings (see google docs).
+    shortSavings: 0.00,//current amount of money in short term savings (however you track finances).
+    longSavings: 0.00,//current amount of money in long term savings (however you track finances).
     totalSavings: 0.00,//total amount of money to be transferred into savings.
     percentages: {donations: 0.00, shortTransfer: 0.00, longTransfer: 0.00, spending: 0.00},//percent of paycheck
     //used for donations, the transfer to short savings, and transfer to long savings respectively. 
@@ -34,25 +34,36 @@ const pay = {
     },
 
     //sets percentages based on certain presets
-    setPercentages(presetSelection){
-        switch (presetSelection){
+    setPercentages(selection){
+        switch (selection){
             case 1:
-                this.percentages = presets[0];
+                this.percentages = this.presets[0];//responsible
                 break;
             case 2: 
-                this.percentages = presets[1];
+                this.percentages = this.presets[1];//little greedy
                 break;
             case 3: 
-                this.percentages.donations = prompt('Donations percentage', 'Enter percentage (e.g. 0.10)');
-                this.percentages.shortTransfer = prompt('Short savings percentage', 'Enter percentage (e.g. 0.20)');    
-                this.percentages.longTransfer = prompt('Long savings percentage', 'Enter percentage (e.g. 0.70)'); 
-                this.percentages.spending = prompt('Spending percentage', 'Enter percentage (e.g 0.00)');   
+                this.percentages = this.presets[2];//goblin
+                break;
+            case 4: //custom
+                this.percentages.donations = Number(prompt('Donations percentage', 'Enter percentage (e.g. 10)')) / 100;
+                this.percentages.shortTransfer = Number(prompt('Short savings percentage', 'Enter percentage (e.g. 20)')) / 100;    
+                this.percentages.longTransfer = Number(prompt('Long savings percentage', 'Enter percentage (e.g. 70)')) / 100; 
+                this.percentages.spending = Number(prompt('Spending percentage', 'Enter percentage (e.g 0)')) / 100;   
+                break;
             default:
                 console.log('Error invalid input');
                 break;
         }
-    }
-  };//end of pay object
+    },
+    
+    //returns new savings to be entered into however you document your finances
+    updatedDocSavings (){
+        let lTran = this.payCheck * this.percentages.longTransfer;
+        let sTran = this.payCheck * this.percentages.shortTransfer;
+        return `Total amount to be transferred into savings: $${this.totalSavings}\n
+        Amount that is for long term savings: $${lTran}\n
+        Amount that is for short term savings: $${sTran}`
+    },
+};//end of pay object
 
-
- 
