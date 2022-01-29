ac3009179fd8baaea46b971391336572da79495b
Axel Galvan <axelgalvan0126@gmail.com>
2022-01-28 17:42:06 -0800

    add presets to pay object

--- script.js
+++ script.js
@@ -3,8 +3,21 @@ const pay = {
     shortSavings: 0.00,//current amount of money in short term savings (see google docs).
     longSavings: 0.00,//current amount of money in long term savings (see google docs).
     totalSavings: 0.00,//total amount of money to be transferred into savings.
-    percentages: {donations: 0.00, shortTransfer: 0.00, longTransfer: 0.00},//percent of paycheck
+    percentages: {donations: 0.00, shortTransfer: 0.00, longTransfer: 0.00, spending: 0.00},//percent of paycheck
     //used for donations, the transfer to short savings, and transfer to long savings respectively. 
+
+    //presets for percentages object, append whenever new preset is needed
+    presets: [
+        {donations: 0.10,
+        shortTransfer: 0.10,
+        longTransfer: 0.80,
+        spending: 0.00},
+        
+        {donations: 0.05,
+        shortTransfer: 0.10,
+        longTransfer: 0.80,
+        spending: 0.05}
+    ],
 
     //calculates and sets total savings by multipliying payCheck by the transfer percentages,
     //also returns calculated total.
@@ -13,4 +26,8 @@ const pay = {
         this.totalSavings = total;
         return total;
     },
+
   };
+
+ 
+ 
